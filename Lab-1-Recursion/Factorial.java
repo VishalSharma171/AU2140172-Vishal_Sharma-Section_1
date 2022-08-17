c656789469a3c885ad5efe5bf99aeef4c8110d99
VishalSharma171 <102252469+VishalSharma171@users.noreply.github.com>
2022-08-17 16:58:39 +0530

    Create Factorial.java

---
+++ Lab-1-Recursion/Factorial.java
@@ -0,0 +1,17 @@
+public class Factorial {
+
+    public static int fact(int n) {
+        if(n==0||n<=0){
+            return 1;
+        }
+        System.out.println(n*fact(n-1));
+        fact(n*(n-1));
+        return fact(n*(n-1));
+        
+    }
+    public static void main(String[] args) {
+        int n=5;
+        fact(n);
+    }
+}
+    
