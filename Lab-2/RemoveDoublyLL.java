c9161dacce966482f2265d835b2db96083849558
VishalSharma171 <102252469+VishalSharma171@users.noreply.github.com>
2022-08-26 22:51:52 +0530

    Add files via upload

---
+++ Lab-2/RemoveDoublyLL.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Vishal Sharma
+ */
+public class RemoveDoublyLL {
+     public static void main(String[] args) {
+        Node_Returns n1 = new Node_Returns(1);
+        Node_Returns n2 = new Node_Returns(12);
+        Node_Returns n3 = new Node_Returns(5);
+        Node_Returns n4 = new Node_Returns(8);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.prev = n3;
+        n3.prev = n2;
+        n2.prev = n1;
+        DoublyLL dll = new DoublyLL(n1);
+        dll.dubbly_remove(dll, 12);
+        dll.display();
+    }
+}
